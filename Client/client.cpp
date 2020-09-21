16e93c54d091fe45f512f7083a106cd3fc457c6c
Roy <roychav99@gmail.com>
2020-09-21 11:06:54 -0600

    Server base y archivo cliente

---
+++ Client/client.cpp
@@ -0,0 +1,14 @@
+#include "client.h"
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
+
+client::client()
+{
+
+}
